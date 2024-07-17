fb6032540f15f7540ff53ecdd5737de6378798ea
Lexicon <iyiolad21@gmail.com>
2024-07-17 13:24:24 +0100

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,27 @@
 import User from "../models/userModel.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
 
 export const createUser = async (req, res)=>{
     try{
+        const { userName, password}= req.body;
         const user = new User(req.body);
+
+        //Check If User Exists In THe Database
+        const existingUser = await User.findOne({ userName});
+        if(existingUser){
+            return res.status(400).json({message: "User Already Exists"});
+        }
+
+        //amount of times salts are generated into the password, makes it harder to crack
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    user.password = hashedPassword;
+
+
         await user.save();
         res.status(201).json({
             status: "success",
@@ -12,3 +31,60 @@ export const createUser = async (req, res)=>{
         res.status(400).send(error);
     }
 }
+
+
+export const userLogin = async (req, res) =>{
+    try{
+        const { userName, password}= req.body;
+
+    //check for password and username
+    if(!userName || !password){
+        return res 
+        .status(404)
+        .json({message: "Please provide a username and password"});
+    }
+
+    //Check If User in the Database
+    const user = await User.findOne({userName});
+    if(!user){
+        return res.status(401).json({message: 'Invaild credentials'});
+    }
+
+    //compare passwords
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword){
+        return res.status(401).json({message: "Invaild credentials"})
+    }
+
+
+    //This is the token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY,{
+        expiresIn: "1h",
+    });
+
+    res.status(200).json({
+        status: "success",
+        message: "User Logged in successfully",
+        token,
+});
+
+
+    }catch (error){
+        res.status(500).json({
+            status: "error",
+            message: "An error occurred while trying to login"
+        });
+    }
+}
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+      // Retrieve all users from the database
+      const users = await User.find({}, { password: 0 }); // Exclude the password field from the response
+      return res.status(200).json({ users });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ message: "Error fetching users"});
+  }
+  };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { createUser } from "../controllers/userController.js"
+import { createUser, getAllUsers, userLogin } from "../controllers/userController.js"
+import { verifyJWToken } from "../middlewares/jwtAuth.js"
 
 const router = express.Router()
 
 router.post("/register", createUser)
+
+router.post("/signIn", userLogin)
+
+router.get("/all",verifyJWToken, getAllUsers)
 
 export default router
